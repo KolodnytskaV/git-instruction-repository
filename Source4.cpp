2fc71c779cfeef3dc26606ec0992cccd9a35fd13
KolodnytskaV <117867385+KolodnytskaV@users.noreply.github.com>
2023-01-19 19:37:09 +0200

    Add files via upload

---
+++ Source4.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <string>
+#include <stdbool.h>
+
+using namespace std;
+using std::wcout;
+
+using std::wstring;
+using std::cin;
+using std::wcin;
+using std::endl;
+
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int x=0;
+	std::cout <<"Змінна х:";
+	std::cin>> x;
+	int y=0;
+	std::cout << "Змінна y:";
+	std::cin>> y;
+
+	
+		
+		if ((x < 10)&&(x + y <=25))
+		{
+			//якщо умова виконана (true)
+			bool isSumAccepted = true;
+				
+		}
+		if (!(x > 10) && !(x + y > 25))
+			bool isSumAccepted = true;
+		else 
+		{
+			//якщо умова не виконана (false)
+			bool isSumAccepted = false;
+		}
+ 
+	
+	return 0;
+	
+
+}
